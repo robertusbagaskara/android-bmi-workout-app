19b70c09addecf10314c989c34d049acddab9c71
Elexita <benhardyanto@gmail.com>
2021-03-28 14:39:48 +0700

    3 design layout asset *Eric

---
+++ app/src/main/java/com/example/ta_pam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ta_pam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
