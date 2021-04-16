501bed1c542f658ec2ae832fa4b800e60d9e864b
robertusbagaskara <robertusbagaskara@gmail.com>
2021-04-16 11:47:11 +0700

    repair button on resep

--- app/src/main/java/com/example/ta_pam/activity_resep.java
+++ app/src/main/java/com/example/ta_pam/activity_resep.java
@@ -1,12 +1,36 @@
 package com.example.ta_pam;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class activity_resep extends AppCompatActivity {
+
+    private ImageButton btnHome, btnHistory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resep);
+        btnHistory = findViewById(R.id.btnHistory);
+        btnHome = findViewById(R.id.btnHome);
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(activity_resep.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(activity_resep.this, activity_history.class);
+                startActivity(i);
+            }
+        });
     }
 }
